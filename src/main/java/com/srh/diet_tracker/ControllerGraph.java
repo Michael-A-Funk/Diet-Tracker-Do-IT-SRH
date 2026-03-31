bac25d5fe25a3de25bcc470dd130c5807007ee09
Michael Alexander Funk <michaelalexanderfunk@gmail.com>
2026-03-31 14:13:02 +0200

    Implemented Code in ControllerGraph, that enables Graph GUI now to represent data for sum calories through all days in which user made enties.

--- src/main/java/com/srh/diet_tracker/ControllerGraph.java
+++ src/main/java/com/srh/diet_tracker/ControllerGraph.java
@@ -1,57 +1,71 @@
 package com.srh.diet_tracker;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class ControllerGraph {
 
+    @FXML
+    private CategoryAxis xAxis;
+    @FXML
+    private NumberAxis yAxis;
+
+    @FXML
+    private LineChart<String,Double> lineChart;
+    @FXML
+    private RadioButton caloriesRadioBtn;
+    @FXML
+    private RadioButton sugarRadioBtn;
+    @FXML
+    private CheckBox meanCheckBox;
+    @FXML
+    private RadioButton sumRadioBtn;
+    @FXML
+    private RadioButton percentageRadioBtn;
+    @FXML
+    private DatePicker olderDatePicker;
+    @FXML
+    private DatePicker newerDatePicker;
+    @FXML
+    private Button graphMakeBtn;
+    private final int CHART_RESOLUTION = 0;
+
     public ControllerGraph(){}
 
-    public void makeGraph(boolean isCalories, boolean isSum, LocalDate olderDate, LocalDate newerDate, Stage stage){
-        double olderDateDouble = olderDate.getDayOfYear();
-        double newerDateDouble = newerDate.getDayOfYear();
-        NumberAxis xAxis = new NumberAxis(olderDateDouble, newerDateDouble, 1);
-        xAxis.setLabel("Tage");
+    public void makeGraph(boolean isCalories, boolean isSum, LocalDate olderDate, LocalDate newerDate){
+
 
         EntryDAO entryDAO = new EntryDAO();
-        NumberAxis yAxis = new NumberAxis(0,entryDAO.getMaxCalories(),5);
-        System.out.println(entryDAO.getMaxCalories());
+        xAxis.setLabel("Zeit (Tage)");
         yAxis.setLabel("Kalorien (kcal)");
-
-        LineChart linechart  = new LineChart(xAxis,yAxis);
 
         XYChart.Series series = new XYChart.Series();
         series.setName("Summe Kalorien aller eingetragenen Zeit");
+        xAxis.setTickLabelRotation(45);
 
         ArrayList<LocalDate> selectedDaysList= entryDAO.returnRegisteredDates(true,null,null);
         ArrayList<Double> sumCaloriesList= entryDAO.returnSUMCaloriesForDateRange(true,null,null);
         for (int i=0; i<selectedDaysList.size();i++){
-            int dayOfYear = selectedDaysList.get(i).getDayOfYear();
-            series.getData().add(new XYChart.Data<>(dayOfYear,sumCaloriesList.get(i)));
+            String day = Integer.toString(selectedDaysList.get(i).getDayOfMonth());
+            series.getData().add(new XYChart.Data<>(day,sumCaloriesList.get(i)));
         }
-
-        linechart.getData().add(series);
-
-        Group root = new Group(linechart);
-
-        Scene scene = new Scene (root, 600,400);
-
-        //Setting title to the Stage
-        stage.setTitle("Line Chart");
-
-        //Adding scene to the stage
-        stage.setScene(scene);
-
-        //Displaying the contents of the stage
-        stage.show();
+        lineChart.getData().clear();
+        lineChart.getData().add(series);
 
     }
 
@@ -60,6 +74,11 @@ public class ControllerGraph {
     }
 
     public void onGraphMakeBtn(ActionEvent actionEvent) {
+        EntryDAO entryDAO = new EntryDAO();
+        ArrayList<Double> caloriesSumList = entryDAO.returnSUMCaloriesForDateRange(true,null,null);
+        ArrayList<LocalDate> datesList = entryDAO.returnRegisteredDates(true, null, null);
+
+        makeGraph(true, true, datesList.getFirst(), datesList.getLast());
 
     }
 }
